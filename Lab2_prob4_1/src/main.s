7293e07e6eb78ea1df17c73a466b91273072a259
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-10-04 14:40:47 +0800

    Added reference, lab2 prob4_1 prob4_2 done, comments included, ready for monte carlo

--- Lab2_prob4_1/src/main.s
+++ Lab2_prob4_1/src/main.s
@@ -1,4 +1,4 @@
-	.syntax unified
+		.syntax unified
 	.cpu cortex-m4
 	.thumb
 .data
@@ -10,18 +10,12 @@
 
 enable_fpu:
 	//Your code start from here
-  	; CPACR is located at address 0xE000ED88
-	LDR.W
-	R0, =0xE000ED88
-	; Read CPACR
-	LDR
-	R1, [R0]
-	; Set bits 20-23 to enable CP10 and CP11 coprocessors
-	ORR
-	R1, R1, #(0xF << 20)
-	; Write back the modified value to the CPACR
-	STR
-	R1, [R0]
+
+	LDR.W R0, =0xE000ED88
+
+	LDR R1, [R0]
+	ORR R1, R1, #(0xF << 20)
+	STR R1, [R0]
 	bx lr
 
 main:
@@ -40,18 +34,19 @@ main:
 	// s2=x-y
 	vsub.f32 s2, s0, s1
 	// s2=x*y
+	vmul.f32 s2, s0, s1
 	// s2=x/y
-	vmul.f32
+	vdiv.f32 s2, s0, s1
 	// load z into r0,
 	ldr r0,=z
 	// copy z from r0 to s2,
-	vldr.f32 s2, [r0]
+	ldr s2, [r0]
 	//u32 a word(4bytes), actually uint32_t (POSIX Standard)
 	// convert z from U32 to float representation F32 in s2
 	//conversion reference to: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0489c/CIHFFGJG.html
 	//usage VCVT{cond}.type Dd, Dm {, #fbits}
 	//now as a self-type conversion
-	vcvt.f32.u32 s2, s2
+	vcvt.f32.u32 s2, s2 //(from right src to left dst)
 	// calculate s3=z+x+y (s2+s0+s1)
 	//s3=x+y
 	vadd.f32 s3, s0, s1
