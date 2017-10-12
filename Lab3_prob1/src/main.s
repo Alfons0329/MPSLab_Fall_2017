15eddd439a3d4f01fab856f918927233489d9e9f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-10-12 23:44:42 +0800

    Commit some files of Lab3

--- Lab3_prob1/src/main.s
+++ Lab3_prob1/src/main.s
@@ -15,7 +15,7 @@ main:
 	LDR	R0, =postfix_expr
 	ldr sp, =user_stack
 	add sp, sp, #128 //open a stack sp, r13
-	bl strlen
+	b postfix_expr_eval
 	//TODO: Setup stack pointer to end of user_stack and calculate the expression using PUSH, POP operators, and store the result into expr_result
 
 
@@ -28,7 +28,7 @@ strlen:
 	//r0 now has postfix address
 	//r1 for the increment i (byte addressable, asciiz a byte for char)
 
-	push {r0} //temporarily store in stack
+	//push {r0} //temporarily store in stack
 	mov r1, #0 //int i=0
 
 	strlen_for_loop: // for(int i=0 arr[i]!='\0' i++)
@@ -43,70 +43,129 @@ strlen:
 		add r1, r1, #1 //i++
 		b strlen_for_loop
 
+	b strlen_quit
+
 strlen_quit:
-	push {r1} //now r1 has the value of how long the string is, stack be like back[r0(addr),r1(strlen)]top
+	//push {r1} //now r1 has the value of how long the string is, stack be like back[r0(addr),r1(strlen)]top
 	bx lr
 
-atoi:
+atoi: //r7 for atoi value r8 for signed_bool_value
     //TODO: implement a “convert string to integer” function
-	//back[r0(addr),r1(strlen)]top
-	pop{r0-r1} //regain
+	/*ex: 1 2 5
+		i ---------> cnt=2
+	    while(cnt)
+		    {
+		        sum+=str[i+cnt]*pow;
+		        pow*=10;
+		        cnt--;
+		    }
+		}
 
+		return sum;
 
+	*/
+	//r9 as counter
+	mov r9, #0
+	for_loop_get_digit:
 
-    bx lr
-/*
-//i pass into atoi
-for(int i=0;i<len;i++)
-{
-    if(!isnotspace(str[i]))//pass into
-    {
-        if(str[i]=='-')
-        {
-            i++;
-            neg_flag=true;
-        }
-        if(neg_flag)
-        {
-            for(int cnt=1;str[i+cnt]!=' ';cnt++);
-        }
-        else
-        {
-            for(int cnt=0;str[i+cnt]!=' ';cnt++);
-        }
+		mov r3, r0
+		add r3, r3, r9
+		ldrb r3, [r3]
+		cmp r3, #32
+		beq atoi_get_value
+		add r9, r9, #1
 
-        break; //goto next_down
-    }
-    break;
-}
-/*cnt=dist(msb,lsb) to check len of a number
-ex: - 1 2 5
-i ---------> cnt =3
-ex: 1 2 5
-i ---------> cnt=2
-*/
-int sum=0,pow=0;
-if(neg_flag)
-{
-    while(cnt>=1)
-    {
-        sum+=str[i+cnt]*pow;
-        pow*=10;
-        cnt--;
-    }
-}
-else
-{
-    while(cnt)
-    {
-        sum+=str[i+cnt]*pow;
-        pow*=10;
-        cnt--;
-    }
-}
+		b atoi_get_value
 
-return sum;
+		atoi_done:
+			cmp r8, #1
+			b ret_positive
+			mov r8, #0
+			sub r8, r8, #1
+			mul r7, r7, r8
 
-*/
+		ret_positive:
+			push {r7}
+bx lr
+atoi_get_value:
+	movs r4, #1 //pow
+	movs r5, #10 //ten
+	movs r7, #0 //sum, or say atoi value
+	while_cnt:
+		mov r3, r0
+		add r3, r3, r9
+		ldrb r3, [r3]
+		sub r3, r3 #48
+		mul r3, r3, r4 //str[i+cnt]*pow;
+		add r7, r7, r4 //sum+=str[i+cnt]*pow;
+		mul r4, r4, r5 //pow*=10
+		sub r9, r9, #1
+		cmp r9, #0
+		beq atoi_done
+
 postfix_expr_eval:
-	strlen:
+	bl strlen
+	/*
+	r0 has the value of where the address of postfix_expr is
+	r1 gets the value of string length
+	r2 serve as counter i
+	r3 to be used as string iterator
+	r4 serves as the current value of the expression evaluation
+	r5, r6 use as two operands for calculation
+	*/
+	//start the for_loop, now r2 is 0
+	LDR	R0, =postfix_expr //regain
+	mov r2, #0
+	expr_eval_for_loop: //for(int i=0 i<len i++)
+		mov r3, r0
+		mov r8, #0 //default neg_flag=0
+		add r3, r3, r2
+		ldrb r3, [r3]
+
+		cmp r3, #48
+		bge get_value
+
+		cmp r3, #32
+		bne is_not_space
+
+		is_not_space:
+			cmp r3, #48
+
+			cmp r3, #45
+			ite eq
+			beq is_negative_sign
+
+			cmp r3, #43
+			bne is_plus_operation
+
+
+		is_negative_sign:
+			add r2, r2, #1 //test if next is a space or not
+			mov r3, r0
+			add r3, r3, r2
+			ldrb r3, [r3]
+			sub r2, r2, #1
+			cmp r3, #32
+			beq is_minus_operation
+			mov r8, #1 //neg_flag=1
+			bl atoi
+			b getvalue_done
+
+		is_minus_operation:
+			pop {r4-r5}
+			add r4, r4, r5
+			push {r4}
+
+		is_plus_operation:
+			pop {r4-r5}
+			sub r4, r4, r5
+			push{r4}
+
+		get_value:
+			bl atoi
+			b getvalue_done
+		getvalue_done:
+		cmp r2, r1
+		blt expr_eval_for_loop
+
+b program_end
