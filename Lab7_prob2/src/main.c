1e7be4ba129bd7c72cb7a3804ee376236f10a940
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-11-27 13:41:38 +0800

    Lab7 prob1 done,prob2 working

--- Lab7_prob2/src/main.c
+++ Lab7_prob2/src/main.c
@@ -29,13 +29,22 @@ void Timer_init( TIM_TypeDef *timer)
 {
     //todo: Initialize timer, dataset from the stm32l476xx.h
     RCC_APB1ENR1 |= RCC_APB1ENR1_TIM2EN; //turn on the timer2
-    TIM2->ARR = 0xFFFFFFFF;
-    TIM2->PSC = 1048576*(/) //prescaler auto reload value after exceed this
-    TIM2->EGR = 
+    TIM2->PSC = 39999U;  //prescaler, how many counter clock cycle  I have to upload my counter
+    TIM2->ARR = 100U; //arr setting the precision of the counter
+    TIM2->EGR = TIM_EGR_UG;  //re-initailzie timer to startup
+
+    //counter will be incremented by one symbolize the millisecond
+    //1 millisecond is 40000/4000000 = 1/100, so each 40000 clock cycle, increse the counter by one
+    //the time precision should be 0.01sec, so set the arr be 99 which means a second should be divieded into 100 parts
+
 }
 void Timer_start(TIM_TypeDef *timer)
 {
     //todo: start timer and show the time on the 7-SEG LED.
+    //enable the counter
+
+    TIM2->CR1 |= TIM_CR1_CEN //Turn on the counter mode,
+
 }
 void display_clr()
 {
